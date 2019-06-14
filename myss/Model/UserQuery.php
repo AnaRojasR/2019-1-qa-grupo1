5b0a788ada186963c166e3e174a55118eccfb0df
steven1097 <stevenpaniagua39@gmail.com>
2019-06-13 18:57:13 -0600

    [Modificación]
    -Execute Query
    -User Query

--- myss/Model/UserQuery.php
+++ myss/Model/UserQuery.php
@@ -69,7 +69,8 @@ class UserQuery
         $query = ['
         FOR x IN user 
         FILTER x.username == @username
-        RETURN {key: x._key, username: x.username, name: x.name, email: x.email, userImage: x.userImage}' => ['username' => $username]];
+        RETURN {key: x._key, username: x.username, name: x.name, email: x.email, birthday: x.birthday, 
+                userImage: x.userImage}' => ['username' => $username]];
 
         $cursor = readCollection($query);
         $resultingDocuments = array();
@@ -82,6 +83,7 @@ class UserQuery
                 $profile['username'] = $resultingDocuments[$key]->get('username');
                 $profile['name'] = $resultingDocuments[$key]->get('name');
                 $profile['email'] = $resultingDocuments[$key]->get('email');
+                $profile['birthday'] = $resultingDocuments[$key]->get('birthday');
                 $profile['key'] = $resultingDocuments[$key]->get('key');
                 $profile['userImage'] = $resultingDocuments[$key]->get('userImage');
             }
@@ -135,11 +137,8 @@ class UserQuery
         if ($dataFound > 0) {
             return true;
         }
-
         return false;
-
     }
-
     // Gets all my friends usernames.
     public static function getAllMyFriends($userId)
     {
@@ -149,7 +148,109 @@ class UserQuery
             'FOR x in follows
             FILTER x._from == @fromUser 
             RETURN x' => ['fromUser' => $userIdComplete]];
-
         return readCollection($statements);
+    }
+
+
+    public static function changeUsername($currentUsername, $newUsername)
+    {
+        $statements = [
+            'FOR u IN user
+            FILTER u.username == @current
+            UPDATE u WITH { username: @new } IN user' => ['current' => $currentUsername, 'new' => $newUsername]];
+        readCollection($statements);
+    }
+
+    public static function changeEmail($currentEmail, $newEmail)
+    {
+        $statements = [
+            'FOR u IN user
+            FILTER u.email == @current
+            UPDATE u WITH { email: @new } IN user' => ['current' => $currentEmail, 'new' => $newEmail]];
+        readCollection($statements);
+    }
+
+    public static function changeName($username, $newName)
+    {
+        $statements = [
+            'FOR u IN user
+            FILTER u.username == @username
+            UPDATE u WITH { name: @name } IN user' => ['username' => $username, 'name' => $newName]];
+        readCollection($statements);
+    }
+
+    public static function changeBirthday($username, $newBirthday)
+    {
+        $statements = [
+            'FOR u IN user
+            FILTER u.username == @username
+            UPDATE u WITH { birthday: @birthday } IN user' => ['username' => $username, 'birthday' => $newBirthday]];
+        readCollection($statements);
+    }
+
+    public static function changePassword($username, $newPassword)
+    {
+        $statements = [
+            'FOR u IN user
+            FILTER u.username == @username
+            UPDATE u WITH { password: @password } IN user' => ['username' => $username, 'password' => $newPassword]];
+        readCollection($statements);
+    }
+
+    public static function changePicture($username, $newPicture)
+    {
+        $statements = [
+            'FOR u IN user
+            FILTER u.username == @username
+            UPDATE u WITH { userImage: @userImage } IN user' => ['username' => $username, 'userImage' => $newPicture]];
+        readCollection($statements);
+    }
+    public static function getAllMyFriendsKeys($userId){
+        $cursor = UserQuery::getAllMyFriends($userId);
+        $userKeys = array();
+        foreach ($cursor as $key => $value){
+            $resultingDocuments[$key] = $value;
+            $userkey = $resultingDocuments[$key]->get('_to');
+
+            array_push($userKeys, $userkey);
+        }
+        return $userKeys;
+
+    }
+
+    public static function searchFriends($username, $userId){
+        $userkeys = UserQuery::getUsersStartingWith($username);
+        $friends = self::getAllMyFriendsKeys($userId);
+
+        $result = array();
+
+        foreach ($userkeys as $ukey => $uvalue){
+            foreach ($friends as $fkey => $fvalue){
+                if($uvalue["key"] == $fvalue){
+                    array_push($result, $uvalue);
+                }
+            }
+        }
+        return $result;
+
+    }
+
+    public static function getUsersStartingWith($username){
+        $username .= "%";
+        $statement = [
+            'FOR u IN user 
+            FILTER u.username LIKE @username
+            RETURN {key: u._id, username: u.username}' => ['username' => $username]
+        ];
+
+        $cursor = readCollection($statement);
+        $userKeys = array();
+        foreach ($cursor as $key => $value){
+            $resultingDocuments[$key] = $value;
+            $userkey['key'] = $resultingDocuments[$key]->get('key');
+            $userkey['username'] = $resultingDocuments[$key]->get('username');
+            array_push($userKeys, $userkey);
+        }
+        return $userKeys;
     }
 }
